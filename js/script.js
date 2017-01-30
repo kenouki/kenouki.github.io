af2909840cb29ff3cf5e0a19c6019b3b7f9d5463
kenouki <ppark6396@gmail.com>
2017-01-30 04:06:27 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function openNav() {
+    "use strict";
+    document.getElementById("horNav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    "use strict";
+    document.getElementById("horNav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "black";
+}
